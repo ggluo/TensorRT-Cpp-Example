fee969fbcc6b1c5e0609fe4c02e4a3ac7d323a8a
Luo, Guanxiong <guanxiong.luo@med.uni-goettingen.de>
2024-03-04 08:18:43 +0100

    update trt_connector.cpp

--- src/trt_connector.cpp
+++ src/trt_connector.cpp
@@ -3,22 +3,26 @@
 
 extern "C" {
 
+inline trt_infer* cast(handle handle_t) {
+    return reinterpret_cast<trt_infer*>(handle_t);
+}
+
 handle createTRT(char** argv) {
     infer_params params{argv[1], 1,  argv[2], ""}; 
     return reinterpret_cast<handle>(new trt_infer(params));
 }
 
 void destoryTRT(handle handle_t) {
-    delete reinterpret_cast<trt_infer*>(handle_t);
+    delete cast(handle_t);
 }
 
 void buildTRT(handle handle_t) {
-    reinterpret_cast<trt_infer*>(handle_t)->build();
+    cast(handle_t)->build();
 }
 
 void executeTRT(handle handle_t) {
-    trt_infer* trt = reinterpret_cast<trt_infer*>(handle_t);
-    (trt)->CopyFromHostToDevice({0.5f, -0.5f, 1.0f}, 0, nullptr);
+    trt_infer* trt = cast(handle_t);
+    (trt)->CopyFromHostToDevice({1.0f, 0.0f, 1.0f}, 0, nullptr);
     (trt)->infer();
     std::vector<float> output(2, 0.0f);
     (trt)->CopyFromDeviceToHost(output, 1, nullptr);
