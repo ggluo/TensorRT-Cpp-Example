5264fa3741fcbc5a3fd127d158d1c10a8da34bf0
gluo <guanxiong.luo@med.uni-goettingen.de>
2024-08-19 09:47:03 +0200

    udpate main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "trt_connector.h"
 #include <cstdio>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
@@ -9,8 +10,13 @@ int main(int argc, char *argv[]) {
     }
 
     handle handle_trt = createTRT(argv);
+
+    std::vector<float> input={0.5f, -0.5f, 1.0f};
+    std::vector<float> output(2, 0.0f);
+
     buildTRT(handle_trt);
-    executeTRT(handle_trt);
+    executeTRT(handle_trt, input, 0, output, 1);
+    std::cout << "Output: " << output[0] << ", " << output[1] << std::endl;
     destoryTRT(handle_trt);
     return 0;
 }
