c8f3580a65397bd2f6a567c1880f9f8da7f2d8d7
Luo, Guanxiong <guanxiong.luo@med.uni-goettingen.de>
2024-08-27 11:09:40 +0200

    update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,38 @@
-echo "Running all tests"
-
-echo "========export the onnx model=========="
+echo -e "*********Running all tests********\n"
+echo -e "==============TEST1===================="
+echo -e "========export the onnx model==========\n"
 python python/export_model.py data/model.onnx
+echo -e "\n"
 
-echo "==========compile the tensorRT inference code=========="
+echo -e "=========================TEST2========================="
+echo -e "==========compile the tensorRT inference code==========\n"
 make 
+echo -e "\n"
 
-echo "==========run the tensorRT inference code from C++ =========="
+echo -e "============================TEST3============================"
+echo -e "==========run the tensorRT inference code from C++ ==========\n"
 ./main data/model.onnx data/first_engine.trt
+echo -e "\n"
 
-echo "==========use the existing engine file to run the tensorRT inference code from C++ =========="
+echo -e "==============TEST4===================="
+echo -e "==========run the tensorRT inference from existing engile file ==========\n"
 ./main data/first_engine.trt
+echo -e "\n"
 
-echo "==========run the tensorRT inference code from C =========="
+echo -e "========================TEST5=============================="
+echo -e "==========run the tensorRT inference code from C ==========\n"
 ./main_c data/model.onnx data/first_engine.trt
+echo -e "\n"
 
-echo "=========run GaussianBlur test=========="
+echo -e "======================TEST6========================="
+echo -e "=========run GaussianBlur test for pytorch==========\n"
 python python/gaussian_blur_pytorch.py data/grd pytorch_blurred data/blur.onnx
 ./gaussian_blur data/blur.onnx data/blur_engine.trt data/grd data/output
 python python/utils.py data/output tensorRT_blurred_pytorch
+echo -e "\n"
 
+echo -e "====================TEST7=============================="
+echo -e "=========run GaussianBlur test for tensorflow==========\n"
 python python/gaussian_blur_tf.py data/grd tf_blurred data/blur.onnx
 ./gaussian_blur data/blur.onnx data/blur_engine.trt data/grd data/output
 python python/utils.py data/output tensorRT_blurred_tf
